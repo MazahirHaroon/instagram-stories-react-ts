db9934415b21005cc2bb2af5bb2cc787a2ac5b13
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-06-20 04:18:28 +0530

    style: add horizontal margin (mx-4) to Logo wrapper

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,6 +1,6 @@
 const Logo = () => {
   return (
-    <div className='w-1/3 h-14'>
+    <div className='w-1/3 h-14 mx-4'>
       <img src='/instagram-logo.png' alt='Instagram' height={150} width={150} />
     </div>
   );
