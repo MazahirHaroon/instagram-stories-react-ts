af91443fb2cce4e388b4eba18489b8fa322f8195
Mazahir Haroon <mazahirharoon@gmail.com>
2025-06-21 00:08:49 +0530

    feat: create close button with svg icon from icons8 and an overlay wrapper component

--- src/components/instagram-ui/index.tsx
+++ src/components/instagram-ui/index.tsx
@@ -2,5 +2,7 @@ import Carousel from './Carousel';
 import StoryThumbnail from './StoryThumbnail';
 import UsernameLabel from './UsernameLabel';
 import StoryViewer from './StoryViewer';
+import Close from './Close';
+import Overlay from './Overlay';
 
-export { Carousel, StoryThumbnail, UsernameLabel, StoryViewer };
+export { Carousel, StoryThumbnail, UsernameLabel, StoryViewer, Close, Overlay };
