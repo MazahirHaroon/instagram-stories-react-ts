6d382aefbe348698dc30c2d56627127882f6b2ea
Mazahir Haroon <mazahirharoon@gmail.com>
2025-06-23 02:06:30 +0530

    feat: add story navigation feature

--- src/components/instagram-ui/StoryViewer.tsx
+++ src/components/instagram-ui/StoryViewer.tsx
@@ -1,16 +1,17 @@
-import type { Story } from '@constants/stories';
-
+import type { Story, StoryAction } from '@constants/stories';
+import { StoryActions } from '@constants/stories';
 import { Overlay, Close } from '@instagram-ui';
 
 interface StoryViewerProps {
   story: Story;
   onClose: () => void;
+  switchStory: (action: StoryAction) => void;
 }
 
-const StoryViewer = ({ story, onClose }: StoryViewerProps) => (
+const StoryViewer = ({ story, onClose, switchStory }: StoryViewerProps) => (
   <div className='fixed inset-0 flex flex-col items-center justify-center bg-black z-[9999]'>
     <Overlay className='h-12'>
-      <span className='absolute top-4 right-2 text-white text-xl cursor-pointer'>
+      <span className='absolute top-4 right-2 text-white text-xl cursor-pointer z-20'>
         <Close onClose={onClose} />
       </span>
     </Overlay>
@@ -21,6 +22,14 @@ const StoryViewer = ({ story, onClose }: StoryViewerProps) => (
         alt={story.alt}
         className='max-h-full max-w-full object-contain'
       />
+      <button
+        onClick={() => switchStory(StoryActions.PREVIOUS)}
+        className='absolute left-0 h-full w-1/2 top-10 z-[9999]'
+      ></button>
+      <button
+        onClick={() => switchStory(StoryActions.NEXT)}
+        className='absolute right-0 h-full w-1/2 top-10 z-[9999]'
+      ></button>
     </div>
 
     <div className='absolute bottom-0 h-12 bg-black w-full' />
