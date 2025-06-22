751ee5fc5f5c923bb75192281523d004211e9914
Mazahir Haroon <mazahir.haroon@bigbinary.com>
2025-06-23 02:08:06 +0530

    refactor: update Carousel to use profilePicture field in Story data

--- src/components/instagram-ui/Carousel.tsx
+++ src/components/instagram-ui/Carousel.tsx
@@ -13,15 +13,15 @@ const Carousel = ({ items, viewStory, showLabel = true }: CarouselProps) => {
   return (
     <div className='flex flex-row gap-4 h-32 px-1.5 scrollbar-none overflow-x-auto overscroll-none scroll-smooth'>
       {items.map((story) => {
-        const { id, src, alt, userName } = story;
+        const { id, profilePicture, userName } = story;
         return (
           <div
             key={id}
             className='flex flex-col gap-1 items-center flex-shrink-0'
           >
             <StoryThumbnail
-              src={src}
-              alt={alt}
+              src={profilePicture}
+              alt={userName}
               viewStory={() => viewStory(story)}
             />
             {showLabel && <UsernameLabel label={userName} />}
