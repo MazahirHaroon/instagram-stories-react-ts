1e76fcebeeb599a45b18b2653ac40abb945d281d
Mazahir Haroon <mazahirharoon@gmail.com>
2025-06-24 23:04:56 +0530

    feat: display avatar and username in StoryViewer overlay header

--- src/components/instagram-ui/StoryViewer.tsx
+++ src/components/instagram-ui/StoryViewer.tsx
@@ -1,11 +1,11 @@
-import type { Story, StoryAction } from '@constants/stories';
+import type { ProfileWithStory } from '@constants/common';
+import type { StoryAction } from '@constants/stories';
 
 import { StoryActions } from '@constants/stories';
 
-import { Overlay, Close } from '@instagram-ui';
-
+import { Overlay, Close, AvatarThumbnail, UsernameLabel } from '@instagram-ui';
 interface StoryViewerProps {
-  story: Story;
+  story: ProfileWithStory;
   onClose: () => void;
   switchStory: (action: StoryAction) => void;
 }
@@ -13,6 +13,19 @@ interface StoryViewerProps {
 const StoryViewer = ({ story, onClose, switchStory }: StoryViewerProps) => (
   <div className='fixed inset-0 flex flex-col items-center justify-center bg-black z-[9999]'>
     <Overlay className='h-12'>
+      <div className='flex gap-1.5 ml-2'>
+        <AvatarThumbnail
+          avatar={story.avatar}
+          userName={story.userName}
+          clickAction={() => () => {}}
+          className='h-3/4 self-center'
+        />
+        <UsernameLabel
+          label={story.userName}
+          className='text-white self-start mt-2'
+        />
+      </div>
+
       <span className='absolute top-4 right-2 text-white text-xl cursor-pointer z-[20]'>
         <Close onClose={onClose} />
       </span>
